59a75c0bd8706920163a77bc793b189d4e77f9bc
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-04-28 19:43:58 +0200

    account selector

---
+++ src/components/AccountSelector.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import web3 from '../web3';
+
+const formStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '40%',
+  maxWidth: 600,
+}
+
+const AccountSelector = React.createClass({
+  getInitialState() {
+    return {
+      accounts: []
+    }
+  },
+  componentWillMount() {
+    const accounts = web3.eth.accounts
+
+    if (accounts[0]) {
+      this.value = accounts[0]
+    }
+
+    this.setState({ accounts });
+  },
+  render() {
+    const { accounts } = this.state;
+    return (
+      <select onChange={(event) => this.value = event.target.value}>
+        {accounts.map(account => <option key={account}>{account}</option>)}
+      </select>
+    )
+  }
+})
+
+export default AccountSelector
--- src/components/CreateAuction.js
+++ src/components/CreateAuction.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import AccountSelector from './AccountSelector'
 
 const formStyle = {
   display: 'flex',
@@ -12,12 +12,17 @@ const CreateAuction = React.createClass({
   handleSubmit(event) {
     event.preventDefault()
 
-    const formFields = {item: this.item.value, price: this.price.value};
+    const formFields = {
+      account: this.account.value,
+      item: this.item.value,
+      price: this.price.value
+    };
     console.log(formFields);
   },
   render() {
     return (
       <form onSubmit={this.handleSubmit} style={formStyle}>
+        <AccountSelector ref={(account) => this.account = account} />
         <input type="text" ref={(item) => this.item = item} placeholder="Item" />
         <input type="number" ref={(price) => this.price = price} placeholder="Price" />
         <button>Create Auction</button>
