ae595f6275179496c03234fdbe939cfbab412aea
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-04-28 18:02:56 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index',
+    'webpack/hot/dev-server'
+  ],
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/public',
+    publicPath: 'build/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test:   /\.css$/,
+        loader: 'style!css!postcss'
+      }
+    ]
+
+  },
+  postcss: function() {
+    return [autoprefixer, precss];
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    contentBase: './public',
+    publicPath: '/build',
+    hot:true,
+    stats: {
+        colors: true,
+        chunks: false
+    },
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+};
