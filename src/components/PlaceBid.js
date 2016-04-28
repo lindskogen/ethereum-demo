acf95ab6dcff1a89b356b2877570f8b20cc8ac7b
Joel Torstensson <oed3933@gmail.com>
2016-04-28 21:36:58 +0200

    Can now place bid.

--- src/components/PlaceBid.js
+++ src/components/PlaceBid.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import web3 from '../web3'
 
 const PlaceBid = React.createClass({
+  handleSubmit(event) {
+    event.preventDefault()
+
+    this.props.auction.placeBid({value: web3.toWei(ParseInt(this.amount.value, 10))})
+  }
   render() {
     const {auction} = this.props;
     return (
